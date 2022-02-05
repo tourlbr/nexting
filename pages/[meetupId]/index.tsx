51b5c8374211d65349c8bed86be16f380a6e4123
Brecht Tourlousse <brecht.tourlousse@studiohyperdrive.be>
2022-02-05 18:28:38 +0100

    feat: add meetup + components

---
+++ pages/[meetupId]/index.tsx
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails(props) {
+	return (
+		<MeetupDetail
+			image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/View_of_Gent_from_St._Michel%27s_Bridge.jpg/532px-View_of_Gent_from_St._Michel%27s_Bridge.jpg"
+			title="First Meetup"
+			address="Some Street 6, Some City"
+			description="This is a first meetup"
+		/>
+	)
+}
