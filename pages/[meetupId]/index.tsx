ea911f8bbb7629706721aefd4abdd8f32bcc870b
Brecht Tourlousse <brecht.tourlousse@studiohyperdrive.be>
2022-02-06 09:52:22 +0100

    feat: implement static props and paths

--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -1,12 +1,54 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 export default function MeetupDetails(props) {
+	const { meetupData } = props;
+
 	return (
 		<MeetupDetail
-			image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/View_of_Gent_from_St._Michel%27s_Bridge.jpg/532px-View_of_Gent_from_St._Michel%27s_Bridge.jpg"
-			title="First Meetup"
-			address="Some Street 6, Some City"
-			description="This is a first meetup"
+			image={meetupData.image}
+			title={meetupData.title}
+			address={meetupData.address}
+			description={meetupData.description}
 		/>
 	)
+}
+
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: "m1"
+				}
+			},
+			{
+				params: {
+					meetupId: "m2"
+				}
+			},
+			{
+				params: {
+					meetupId: "m3"
+				}
+			}
+		]
+	}
+}
+
+export async function getStaticProps(context) {
+	const { params } = context;
+	const meetupId = params.meetupId;
+
+	return {
+		props: {
+			meetupData: {
+				id: meetupId,
+				image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/View_of_Gent_from_St._Michel%27s_Bridge.jpg/532px-View_of_Gent_from_St._Michel%27s_Bridge.jpg",
+				title: "First Meetup",
+				address: "Some Street 6, Some City",
+				description: "This is a first meetup",
+			}
+		}
+	}
 }
